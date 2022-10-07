3caf18b5fcabf5a3edd569ffe59756733e3509de
gonza rolo <gonzarolo22@gmail.com>
2022-10-07 20:08:59 -0300

    se unio ale

--- src/juego/Rectangulo.java
+++ src/juego/Rectangulo.java
@@ -19,11 +19,13 @@ public class Rectangulo {
 		this.ancho=100;
 		this.largo=200;
 		this.angulo=0;
-		this.color=Color.BLACK;
+		this.color=Color.white;
 }
 	
 	public void dibujarRectangulo(Entorno e) {
 		e.dibujarRectangulo(this.x, this.y, this.ancho, this.ancho, this.angulo, this.color);
 	}
 
+	
+	
 }
