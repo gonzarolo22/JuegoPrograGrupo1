cf1958327a3db64355f45588ea1de7b7d8a6142d
Correa Alejandro <correa92.dev@gmail.com>
2022-10-10 14:27:36 -0300

    se agrega clase arbol

--- src/juego/Arbol.java
+++ src/juego/Arbol.java
@@ -1,10 +1,40 @@
 package juego;
 
+import java.awt.Color;
+import java.util.Random;
+
+import entorno.Entorno;
 public class Arbol {
 	
-	int alto;
-	int ancho;
+	private int x;
+	private int y;
+	private int ancho;
+	private int alto;
+	private int angulo;
+	private Color color;
+	
+	
+	public Arbol(int x, int y) {
+		this.x=x;
+		this.y=y;
+		this.ancho=100;
+		this.alto=5;
+		this.angulo=0;
+		this.color=Color.GRAY;
+}// Arbol
+	
+	public void dibujarArbol(Entorno e) {
+		
+		Random rand1 = new Random();
+		  System.out.println(rand1.nextInt(4) + 1);
+		e.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, this.angulo, this.color);
+		
+	}//crearArbol
+	
+	public void desplazar() {
+		this.x-=1;
+	}
 	
 	
 
-}
+}// class Arbol
--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -9,9 +9,9 @@ public class Juego extends InterfaceJuego {
 
 	
 	private Entorno entorno;
-	
 	private Mono mono;
 	private Suelo suelo;
+	private Arbol arbol;
 	
 	public Juego() {
 		this.entorno = new Entorno(this, "Escape del mono - Grupo 1 - Correa A - Rolon G - Bentacor L - V0.01", 800, 600);
@@ -19,7 +19,7 @@ public class Juego extends InterfaceJuego {
 		this.entorno.iniciar();
 		this.suelo= new Suelo(entorno,entorno.ancho()/2); 
 		this.mono= new Mono(0,entorno.alto()-suelo.alto);
-		
+		this.arbol = new Arbol(entorno.ancho()+100,300);
 		
 	}
 
@@ -30,13 +30,16 @@ public class Juego extends InterfaceJuego {
 		
 		suelo.dibujarRectangulo(entorno);
 		mono.dibujarMono(entorno);
+		arbol.dibujarArbol(entorno);
+		arbol.desplazar();
+//		if(entorno.estaPresionada(entorno.TECLA_ARRIBA)) {			
+//			mono.saltar(entorno);
+//		}else {
+//			mono.gravedad(2);
+//		}
 		
-		if(entorno.estaPresionada(entorno.TECLA_ARRIBA)) {			
-			mono.saltar(entorno);
-		}else {
-			mono.gravedad(2);
-		}
 		
+//		
 		
 	}
 	
