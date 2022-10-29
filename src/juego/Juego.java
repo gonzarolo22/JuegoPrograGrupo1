53f49db930b7745931bce5babf9bfbb212e565e7
Correa Alejandro <correa92.dev@gmail.com>
2022-10-29 00:13:16 -0300

    se corrige detalles gamer over

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -23,8 +23,8 @@ public class Juego extends InterfaceJuego {
 
 //
 	public Juego() {
-		
-		this.entorno = new Entorno(this, "Escape del mono - Grupo 1 - Correa A - Rolon G - Bentacor L - V0.01", 800,600);
+
+		this.entorno = new Entorno(this, "Escape del mono - Grupo 1 - Correa A - Rolon G - Bentacor L - V0.01", 800, 600);
 		this.punto = 0;
 		this.vida = 3;
 		this.selva = new Selva[2];
@@ -39,6 +39,8 @@ public class Juego extends InterfaceJuego {
 		this.tigre = new Tigre[2];
 		this.serpiente = new Serpiente[2];
 
+	
+		// se crean el fondo de pantalla, los tigres, arboles y las serpientes
 		Selva.iniciaSelva(selva);
 
 		Arbol.crearArboles(this.arbol, entorno);
@@ -51,15 +53,12 @@ public class Juego extends InterfaceJuego {
 		this.entorno.iniciar();
 	}// juego
 
-
-
 	public void tick() {
 
 		suelo.dibujarRectangulo(entorno);
 		Selva.dibujarFondo(selva, entorno);
 		// Procesamiento de un instante de tiempo.
 		if (vida > 0) {
-		
 
 			if (this.itemPiedra.saleDePantalla()) {
 				this.itemPiedra.crearPiedras();
@@ -95,110 +94,113 @@ public class Juego extends InterfaceJuego {
 
 			// condiciones de los arboles
 			for (int i = 0; i < arbol.length; i++) {
-				if(arbol[i]!=null) {
-				arbol[i].dibujarArbol(entorno);
-				arbol[i].desplazar();
+				if (arbol[i] != null) {
+					arbol[i].dibujarArbol(entorno);
+					arbol[i].desplazar();
 
-				if (mono.chocaConArbol(arbol[i]) && arbol[i].isDioPuntos()==false) {
-					punto+=5;
-					arbol[i].setDioPuntos(true);
-				}
+					if (mono.chocaConArbol(arbol[i]) && arbol[i].isDioPuntos() == false) {
+						punto += 5;
+						arbol[i].setDioPuntos(true);
+					}
 
-				if (arbol[i].saleDePantalla()) {
-					// si sale de la pantalla sobreescribo el arbol con uno nuevo
-					arbol[i] = null;
-					Arbol.crearArboles(this.arbol, entorno);
-				}
+					if (arbol[i].saleDePantalla()) {
+						// si sale de la pantalla sobreescribo el arbol con uno nuevo
+						arbol[i] = null;
+						Arbol.crearArboles(this.arbol, entorno);
+					}
 				}
 			}
 
 			// condiciones de los tigres
 			for (int i = 0; i < tigre.length; i++) {
-				if(tigre[i]!=null) {
-				tigre[i].dibujarTigre(entorno);
-				tigre[i].desplazar();
+				if (tigre[i] != null) {
+					tigre[i].dibujarTigre(entorno);
+					tigre[i].desplazar();
 
-				if (tigre[i].saleDePantalla()) {
-					tigre[i] = null;
-					Tigre.agregaTigre(tigre, entorno, suelo);
-				}
-				if (mono.chocaConTigre(tigre[i]) && tigre[i].isPerdioVida()==false) {
-						vida-=1;
+					if (tigre[i].saleDePantalla()) {
+						tigre[i] = null;
+						Tigre.agregaTigre(tigre, entorno, suelo);
+					}
+					if (mono.chocaConTigre(tigre[i]) && tigre[i].isPerdioVida() == false) {
+						vida -= 1;
 						tigre[i].setPerdioVida(true);
-					
-				}
-				if (tigre[i].chocaConPiedra(piedra)) {
-					tigre[i]=null;
-					Tigre.agregaTigre(tigre, entorno, suelo);
-				}
+
+					}
+					if (tigre[i].chocaConPiedra(piedra)) {
+						tigre[i] = null;
+						Tigre.agregaTigre(tigre, entorno, suelo);
+					}
 				}
 			}
 
 			// condiciones de las serpientes
 			for (int i = 0; i < serpiente.length; i++) {
-				if(serpiente[i]!=null) {
-				serpiente[i].dibujarSerpiente(entorno);
-				serpiente[i].desplazar();
+				if (serpiente[i] != null) {
+					serpiente[i].dibujarSerpiente(entorno);
+					serpiente[i].desplazar();
 
-				if (serpiente[i].saleDePantalla()) {
-					serpiente[i] = null;
-					Serpiente.agregaSerpiente(this.serpiente, this.arbol);
-				}
+					if (serpiente[i].saleDePantalla()) {
+						serpiente[i] = null;
+						Serpiente.agregaSerpiente(this.serpiente, this.arbol);
+					}
 
-				if(mono.chocaConSerpiente(serpiente[i]) && serpiente[i].isPerdioVida()==false) {
-					vida-=1;
-					serpiente[i].setPerdioVida(true);
+					if (mono.chocaConSerpiente(serpiente[i]) && serpiente[i].isPerdioVida() == false) {
+						vida -= 1;
+						serpiente[i].setPerdioVida(true);
+					}
+
+					if (serpiente[i].chocaConPiedra(piedra)) {
+						serpiente[i] = null;
+						Serpiente.agregaSerpiente(this.serpiente, this.arbol);
+					}
 				}
-				
-				
-				if (serpiente[i].chocaConPiedra(piedra)) {
-					serpiente[i]=null;
-					Serpiente.agregaSerpiente(this.serpiente, this.arbol);
-				}}
-				}
+			}
 			for (int i = 0; i < piedra.length; i++) {
 				if (piedra[i] != null) {
 					piedra[i].dibujarPiedra(entorno);
 					piedra[i].avanzar();
-					if(piedra[i].saleDePantalla(entorno)){
-						piedra[i]=null;
+					if (piedra[i].saleDePantalla(entorno)) {
+						piedra[i] = null;
 					}
 				}
 			}
 
 			if (entorno.sePresiono(entorno.TECLA_ESPACIO)) {
-				if(mono.getDisparosDisp()>0) {
-				Piedra.agregarPiedra(piedra, mono);
-				
+				if (mono.getDisparosDisp() > 0) {
+					Piedra.agregarPiedra(piedra, mono);
+
 				}
 			}
 
 			mono.dibujarMono(entorno);
 			itemPiedra.dibujarPiedras(entorno);
 			itemPiedra.desplazar();
-			entorno.cambiarFont(" ", 25, Color.red);
-			entorno.escribirTexto("puntaje:"+punto,600,50) ;
-			entorno.cambiarFont(" ", 25, Color.RED);
-			entorno.escribirTexto("vidas:"+vida,50,50) ;
+			entorno.cambiarFont(" ", 26, Color.red);
+			entorno.escribirTexto("PUNTAJE: " + punto, 600, 50);
+			entorno.cambiarFont(" ", 26, Color.RED);
+			entorno.escribirTexto("VIDAS: " + vida, 50, 50);
 		} else {
-			entorno.dibujarImagen(fondo,entorno.ancho()/2,entorno.alto()/2, 0, 1);
-			entorno.cambiarFont(" ", 35, Color.red);
-			entorno.escribirTexto("apreta la tecla [ENTER] para volver a empezar",50,500) ;
+			entorno.dibujarImagen(fondo, entorno.ancho() / 2, entorno.alto() / 2, 0, 1.35);
+			entorno.cambiarFont(" ", 26, Color.red);
+			entorno.escribirTexto("Presione [ENTER] para volver a empezar", 160, 550);
+			
 			if (entorno.estaPresionada(entorno.TECLA_ENTER)) {
-				vida+=3;
+				vida += 3;
 				punto = 0;
 				mono.setDisparosDisp(3);
-				for (int i=0;i<tigre.length;i++) {
-				if(tigre[i]!=null) {
-					tigre[i] = null;
-					Tigre.agregaTigre(tigre, entorno, suelo);
-				}}
-				for ( int i=0;i<serpiente.length;i++) {
-					if(serpiente[i]!=null) {
+				for (int i = 0; i < tigre.length; i++) {
+					if (tigre[i] != null) {
+						tigre[i] = null;
+						Tigre.agregaTigre(tigre, entorno, suelo);
+					}
+				}
+				for (int i = 0; i < serpiente.length; i++) {
+					if (serpiente[i] != null) {
 						serpiente[i] = null;
 						Serpiente.agregaSerpiente(this.serpiente, this.arbol);
-					}}
-		}
+					}
+				}
+			}
 		}
 	}// fin tick()
 
